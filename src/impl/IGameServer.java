666c35de9b52719a4e4549488b5564751694c4bd
Chen, Yiqun <feedingfranzy@gmail.com>
2014-11-04 22:07:09 +0100

    Adding makefile
    Since the project is required to be worked under the Apache© Ant the
    build.xml is fulfilled today. It creates the ./bin folder as the build
    folder. Then all compiled classes are redirected to that folder.
    The target "run" still doesn't work so far.
    The interface class(the impl package) should be compiled into jar in the
    future.
    
    Signed-off-by: Chen, Yiqun

--- src/impl/IGameServer.java
+++ src/impl/IGameServer.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IGameServer extends Remote{
-	public void login (String playerName, IGameClient client) throws RemoteException;
+	public void login(String playerName, IGameClient client) throws RemoteException;
 	public void logout(String playerName) throws RemoteException;
 	public void huntFly(String playerName) throws RemoteException;
 }
